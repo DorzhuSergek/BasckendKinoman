2aa10363cf18258545a0193c1d1c6cc268dd7480
DorzhuSergek <zairovd340@gmail.com>
2022-04-11 23:49:41 +0600

    add couple field

--- src/model.py
+++ src/model.py
@@ -8,3 +8,10 @@ class Movie(Base):
     __tablename__ = "Movies"
     id = Column(Integer, primary_key=True, index=True)
     Name = Column(String, unique=True, index=True)
+    Poster = Column(String, unique=True, index=True)
+    Genre = Column(String, unique=True, index=True)
+    RaitingIMDb = Column(String, unique=True, index=True)
+    sinopsis = Column(String, unique=True, index=True)
+    Trailer = Column(String, unique=True, index=True)
+    Vote_from_user = Column(String, unique=True, index=True)
+    typeMovies = Column(String, unique=True, index=True)
--- src/schemas.py
+++ src/schemas.py
@@ -3,6 +3,13 @@ from pydantic import BaseModel
 
 class MovieBase(BaseModel):
     name: str
+    poster: str
+    genre: str
+    raitingIMDb: str
+    sinopsis: str
+    trailer: str
+    vote_from_user: str
+    typeMovies: str
 
 
 class Movie(MovieBase):
